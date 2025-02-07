6a0ff207b7b9be95f37848feeb58130fc798b96f
auto_1 <alexch@yandex.ru>
2025-02-07 13:31:19 +0300

    10.6 Практическая работа. Массивы Задание №3 готово

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        while (true) {
+            System.out.println("Введите число: ");
+            String text = new Scanner(System.in).nextLine();
+            int drawing = Integer.parseInt (text.trim ());
+            TwoDimensionalArray.getTwoDimensionalArray(drawing);
 
+        }
     }
 }
--- src/TwoDimensionalArray.java
+++ src/TwoDimensionalArray.java
@@ -1,3 +1,30 @@
+import java.util.Arrays;
+
 public class TwoDimensionalArray {
 
+    public static char[][] getTwoDimensionalArray(int size) {
+        char[][] twoArray = new char[size][];
+        for (int i = 0; i < twoArray.length; i++) {
+            twoArray[i] = new char[size];
+        }
+
+        for (int i = 0; i < twoArray.length; i++) {
+            for (int j = 0; j < twoArray[i].length; j++) {
+                twoArray[i][j] = (i == j) ? 'X' : ' ';
+            }
+        }
+
+        for (int i = 0; i < twoArray.length; i++) {
+            twoArray[i][(twoArray.length - 1) - i] = 'X';
+        }
+
+        for (int i = 0; i < twoArray.length; i++) {
+            for (int j = 0; j < twoArray[i].length; j++) {
+                System.out.print(twoArray[i][j]);
+                System.out.print(j < (twoArray[i].length - 1)  ? "" :"\n" );
+            }
+        }
+
+        return twoArray;
+    }
 }
